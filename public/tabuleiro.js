6b572d31afa1bef9c45b9e0c69a3e893e7f82620
Melquisedeque Pereira Franca <melquifranca@hotmail.com>
2020-03-21 17:31:30 -0300

    Ajuste porta IO

--- public/tabuleiro.js
+++ public/tabuleiro.js
@@ -628,7 +628,7 @@ function atualizaStatusJogo(data) {
     atualizaBancoPecas(data.bancoPecas);
     atualizaTabuleiro(data.tabuleiro);
 }
-const socket = io("http://192.168.1.4:3333");
+const socket = io(process.env.PORT ? '' : "http://192.168.1.4:3333");
 socket.on("connect", async function() {
     socket.emit("nova-sala", window.localStorage.sala);
 
